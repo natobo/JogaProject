a35891fecb03fd4062b46f0db934c0c2b0534173
natobo <na.tobo@uniandes.edu.co>
2021-10-05 22:58:02 -0500

    inicio y nuestros servicios avances

---
+++ front-noframework/js/video.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+const lightbox = GLightbox({
+  href: 'https://www.youtube.com/watch?v=QAz07vzN5Nk',
+  type: 'video',
+  source: 'youtube', // vimeo, youtube or local
+  width: 900,
+});
