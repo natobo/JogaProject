f070a64fbbe790553ff1a93ec615416da20e4ccd
necalero <ne.calero@uniandes.edu.co>
2021-10-09 22:05:20 -0500

    Feature completa

--- front-noframework/js/templates.js
+++ front-noframework/js/templates.js
@@ -2,8 +2,8 @@ export const getCardTemplate = (game) => `
 <div class="card game" style="width: 18rem;">
     <img src="${game.linkPortada}" class="card-img-top" alt="...">
     <div class="card-body game-description">
-      <h5 class="card-title">${game.nombre}</h5>
-      <p class="card-text">${game.descripcion}</p>
+      <h5 class="card-title">${game.name}</h5>
+      <p class="card-text">${game.description}</p>
       <a href="#" class="btn btn-primary ">Encontrar grupo</a>
     </div>
 </div>`;
