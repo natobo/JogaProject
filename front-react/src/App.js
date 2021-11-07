b2198b8fdc3bc9e1ed9fcb06555f9e13d39258f5
necalero <ne.calero@uniandes.edu.co>
2021-11-06 23:42:58 -0500

    Routing despues de lagrimas y sudor

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Landing } from './components/landing/Landing';
+import { Home } from './components/home/Home';
+import { Chats } from './components/chats/Chats';
+import { Juegos } from './components/juegos/Juegos';
+import { Lfgs } from './components/lfgs/Lfgs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/chats">
+          <Chats />
+        </Route>
+        <Route exact path="/juegos">
+          <Juegos />
+        </Route>
+        <Route exact path="/lfgs">
+          <Lfgs />
+        </Route>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+      </Router>
+    </>
   );
 }
 
---
+++ front-react/src/components/lfgs/Lfgs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Lfgs = () => (
+  <div>
+    <h1>Lfgs</h1>
+  </div>
+);
