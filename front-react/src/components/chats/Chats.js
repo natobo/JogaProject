aad813366426c2cf21591c19dc9768198446dba6
Sebastian Rosado <s.rosado@uniandes.edu.co>
2021-11-08 00:42:09 -0500

    ChatReact

--- front-react/src/components/chats/Chats.js
+++ front-react/src/components/chats/Chats.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './Chats.scss';
 
 export const Chats = () => (
-  <div>
-    <h1>Chats</h1>
-  </div>
+  <>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light menu shadow fixed-top">
+        <div className="container-fluid">
+          {/* <a className="navbar-brand">
+            <img src={jogaLogo} alt="Logo Joga" />
+          </a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  </>
 );
