4962d67fb4688e93c72524ef0e6e1d5d99953d3c
necalero <ne.calero@uniandes.edu.co>
2021-11-09 11:24:11 -0500

    porfavor

--- front-react/src/components/juegos/title/TitleSearch.js
+++ front-react/src/components/juegos/title/TitleSearch.js
@@ -2,18 +2,9 @@ import React, { useState } from 'react';
 import './TitleSearch.scss';
 
 function TitleSearch() {
-  const [searchTerm, setSearchTerm] = useState('');
   return (
     <div className="juegos-titulo">
       <div className="text">Juegos</div>
-      <input
-        className="busqueda"
-        type="text"
-        placeholder="Buscar..."
-        onChange={(event) => {
-          setSearchTerm(event.target.value);
-        }}
-      />
     </div>
   );
 }
