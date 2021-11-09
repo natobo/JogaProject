fb4bfdaa2db03bae3cdc2147c992df5588d06e6b
Nicolás Andrés Tobo Urrutia <47015612+natobo@users.noreply.github.com>
2021-11-09 11:11:54 -0500

    Revert "Se adiciona la posibilidad de filtrar en carrusel por tags"

--- front-react/src/components/juegos/title/TitleSearch.js
+++ front-react/src/components/juegos/title/TitleSearch.js
@@ -1,21 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './TitleSearch.scss';
 
-function TitleSearch() {
-  const [searchTerm, setSearchTerm] = useState('');
-  return (
-    <div className="juegos-titulo">
-      <div className="text">Juegos</div>
-      <input
-        className="busqueda"
-        type="text"
-        placeholder="Buscar..."
-        onChange={(event) => {
-          setSearchTerm(event.target.value);
-        }}
-      />
-    </div>
-  );
-}
-
-export default TitleSearch;
+export const TitleSearch = () => (
+  <div className="juegos-titulo">
+    <div className="text">Juegos</div>
+  </div>
+);
