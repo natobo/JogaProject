b5aa5ac8da07c9328cc80271a09d126575c15845
necalero <ne.calero@uniandes.edu.co>
2021-11-10 11:00:06 -0500

    se elimina carpeta duplicada de juegos

--- front-react/src/components/juegoss/title/TitleSearch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './TitleSearch.scss';
-
-export const TitleSearch = () => (
-  <div className="juegos-titulo">
-    <div className="text">Juegos</div>
-  </div>
-);
